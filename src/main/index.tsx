93c0976c316ea0345c91dc83b06ceb9c2f3be0f3
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-19 20:57:32 -0300

    adiciona o 'Sass' (Syntactically Awesome Style Sheets) nas dependências do projeto e renomeia os arquivos '.css' para '.scss'

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../presentation/styles/index.css'
+import '../presentation/styles/index.scss'
 import App from '../App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
