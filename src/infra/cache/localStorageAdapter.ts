7ff7215afa7f29cd2e3406dbf7ddc8211576c3da
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-26 16:36:26 -0300

    cria adaptador que implementa as  interfaces 'getter' e 'setter' para manipularem o armazenamento local

---
+++ src/infra/cache/localStorageAdapter.ts
@@ -0,0 +1,15 @@
+import { IGetStorage, ISetStorage } from "../../data/protocols/cache";
+
+export class LocalStorageAdapter implements IGetStorage, ISetStorage {
+    set(key: string, value: object): void {
+        if (value) {
+            localStorage.setItem(key, JSON.stringify(value));
+        } else {
+            localStorage.removeItem(key);
+        }
+    }
+
+    get(key: string): any {
+        return JSON.parse(localStorage.getItem(key) || '{}');
+    }
+}
