efa7abfd7fa5637baeb1f14e48f9697e4db26698
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-26 23:01:51 -0300

    cria 'GameModel' e altera local do 'GameMinModel' e 'StarRatingModel'

---
+++ src/domain/models/gameModel.ts
@@ -0,0 +1,15 @@
+export type GameModel = {
+
+}
+
+export type GameMinModel = {
+    id: number,
+    title: string,
+    score: number,
+    imgUrl: string,
+    position: number,
+};
+
+export type StarRatingModel = {
+    score: number
+}
