3b0eb4a4853f3f8eb17dc9b55c64306673a722d9
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-07-31 15:55:10 -0300

    [FIX] - remove contexto criado erroneamente na camada de 'presentation'

--- src/presentation/contexts/colorScheme.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-import { IColorSchemeContextModel } from '../../domain/models/colorSchemeModel';
-import { initState } from '../hooks/colorSchemeProvider';
-
-export const ColorSchemeContext = createContext<IColorSchemeContextModel>({
-    mode: initState.isDark,
-    dispatch: () => {}
-});
