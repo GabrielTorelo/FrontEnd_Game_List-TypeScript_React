498d094cce93290326f91388f0ccfc1faacb91f7
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-08-03 15:46:51 -0300

    envia pela rota '/' para o componente 'Home', props do esquema de cor e o método para mudá-lo

--- src/main/routes/index.tsx
+++ src/main/routes/index.tsx
@@ -1,16 +1,22 @@
+import { useContext } from "react";
 import { BrowserRouter, Routes as NavRoutes, Route } from "react-router-dom";
+import { ColorSchemeContext } from "../factories/contexts/colorScheme";
 import Home from "../../presentation/pages/home";
 
 const Routes = () => {
-  return (
-    <BrowserRouter>
-      <NavRoutes>
-        <Route path="/">
-          <Route index element={<Home />} />
-        </Route>
-      </NavRoutes>
-    </BrowserRouter>
-  );
+    const theme = useContext(ColorSchemeContext);
+    const mode = theme.mode;
+    const dispatch = theme.dispatch;
+
+    return (
+        <BrowserRouter>
+            <NavRoutes>
+                <Route path="/">
+                    <Route index element={<Home mode={mode} dispatch={dispatch} />} />
+                </Route>
+            </NavRoutes>
+        </BrowserRouter>
+    );
 };
 
 export default Routes;
