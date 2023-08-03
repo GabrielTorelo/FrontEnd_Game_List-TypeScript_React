ed725da79b10e825d5317d025f98b9bce15311c7
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-08-03 15:12:20 -0300

    recebe e envia para 'Header' props com o esquema de cor e o método para mudá-lo

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { IColorSchemeContextModel } from "../../../domain/models/colorSchemeModel";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import CardGameList from "../../components/CardGameList";
 import './styles.scss'
 
-type HomeProps = {};
-
-const Home: React.FC<HomeProps> = () => {
+const Home: React.FC<IColorSchemeContextModel> = ({ mode, dispatch }) => {
     return (
         <div className="container">
-            <Header />
+            <Header mode={mode} dispatch={dispatch} />
             <div className="lists">
                 <CardGameList id={1} name="Todos os Jogos" />
             </div>
