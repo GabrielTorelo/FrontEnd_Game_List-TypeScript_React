0192e793895adc98135c0f2da7c7119f45f4adfa
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-08-10 17:47:04 -0300

    implementa modelos da lista de jogos (gameListModel)

--- src/domain/models/gameListModel.ts
+++ src/domain/models/gameListModel.ts
@@ -1,4 +1,23 @@
+import { GameMinModel, GameCollectionModel } from "./gameModel";
+
 export type GameListModel = {
     id: number,
     name: string
 };
+
+export type GameListIDModel = {
+    id: number
+};
+
+export type GameListAllModel = {
+    lists: GameListCollectionModel,
+    games: GameCollectionModel[]
+
+};
+
+export type GameListAndGamesModel = {
+    list: GameListModel,
+    games: GameMinModel[]
+}
+
+export type GameListCollectionModel = GameListModel[];
