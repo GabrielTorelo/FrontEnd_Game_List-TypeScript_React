9ffb7018f9c47a493c6cf89e36a3404e166484e4
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-08-10 23:34:18 -0300

    implementa o 'IGetGameLists'

---
+++ src/main/factories/usecases/makeRemoteGetGameLists.ts
@@ -0,0 +1,23 @@
+import { RemoteGetGameLists } from '../../../data/usecases/remoteGetGameLists';
+import { RemoteGetGamesOfList } from '../../../data/usecases/remoteGetGamesOfList';
+import { GameListIDModel } from '../../../domain/models/gameListModel';
+import { IGetGameLists, IGetGamesOfList } from '../../../domain/usecases/gameList/getGameLists';
+import { makeAxiosHttpClient, makeApiUrl } from '../http';
+
+export const makeRemoteGetGameLists = (): IGetGameLists => {
+    const remoteGetGameLists = new RemoteGetGameLists(
+        makeApiUrl('/lists'),
+        makeAxiosHttpClient()
+    );
+
+    return remoteGetGameLists;
+};
+
+export const makeRemoteGetGamesOfList = ({ id }: GameListIDModel): IGetGamesOfList => {
+    const remoteGetGamesOfList = new RemoteGetGamesOfList(
+        makeApiUrl(`/lists/${id}/games`),
+        makeAxiosHttpClient()
+    );
+
+    return remoteGetGamesOfList;
+};
